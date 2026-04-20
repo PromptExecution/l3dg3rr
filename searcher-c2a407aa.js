3e23b5166ca5e686445f759bfdf0f403815a6b4d
promptexecutionerr <promptexecutionerr@users.noreply.github.com>
2026-04-20 10:07:24 +0000

    deploy: aaa18a6d152fd27deb829dd8fc55a25d60d91359

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c80b5533.js',
+                path_to_root + 'searchindex-88c50e64.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
